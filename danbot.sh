51a3bd25d6bc02c795e1d651b5d237d9b0714842
Unknown <danheisei4@gmail.com>
2020-01-28 20:04:46 +0000

    updated bash

--- danbot.sh
+++ danbot.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd /home/pi/Projects/noobdanbot/DanBot2/
 git pull
-python tgAdapter.py
+x-terminal-emulator -e python3 tgAdapter.py
+
+cd /discord/danddanbot/
+python3 danddanbot.py
 
 $SHELL
