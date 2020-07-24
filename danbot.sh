4b1473507663ab2247ad9eb8fb6e0f84be9c1341
kikones34 <kikones34@hotmail.es>
2020-07-24 17:34:41 +0200

    Fix startup script

--- danbot.sh
+++ danbot.sh
@@ -2,7 +2,7 @@
 
 cd /home/pi/Projects/noobdanbot/DanBot2/
 git pull
-x-terminal-emulator -e /usr/bin/python3 tgAdapter.py
+x-terminal-emulator -e /usr/bin/python3 tg_adapter.py
 
 cd /home/pi/Projects/noobdanbot/DanBot2/danddanbot/
 /usr/bin/python3 danddanbot.py
